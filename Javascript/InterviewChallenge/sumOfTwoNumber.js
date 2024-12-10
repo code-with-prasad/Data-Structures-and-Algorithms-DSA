b0e723964d588b388e6aae6b15fb4e91d9ad8aab
M prasad <mandaprasad18@gmail.com>
2024-12-10 11:59:59 +0530

    Sum of Two Numbers
    I would take two parameters and the following function can be used to calculate the sum of any 2 numbers that are passed as arguments .

---
+++ Javascript/InterviewChallenge/sumOfTwoNumber.js
@@ -0,0 +1,9 @@
+
+// 1. Write a JavaScript function to calculate the sum of two numbers.
+
+
+function sumOfTwoNumbers(num1, num2){
+    console.log("addition a + b  = ", num1 + num2);
+}
+
+sumOfTwoNumbers(5, 9)
